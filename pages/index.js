41f48dbdd9d4e0fa81b21f1fa846094670f07036
FarihulR <farihulrouf@gmail.com>
2021-01-21 11:32:15 +0700

    six silver

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div className="container mx-auto mt-4">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Saya Halaman Utama</h1>
+       </main>
+
+    </div>
+  )
+}
